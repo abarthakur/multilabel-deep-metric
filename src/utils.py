9e27c418c226007466d25a54048ad2c3f8706253
Aneesh Barthakur <abarthakur96@gmail.com>
2020-03-23 21:10:46 +0530

    Triplet mining version 2

--- src/utils.py
+++ src/utils.py
@@ -19,46 +19,56 @@ def make_batches(x_mat,y_mat,batch_size,shuffle=True):
 	return train_dataset
 
 
-def select_triplets(embeddings,y_batch,threshold_similarity,num_positives,max_negatives,debug=False):
+def select_triplets(embeddings,y_batch,max_negatives_per_pos,max_trips_per_anchor,debug=False):
 	triplets=[]
 	for i in range(0,embeddings.shape[0]):
 		anchor=embeddings[i,:]
 		anchor_y=y_batch[i,:]
+		# get similarity scores
 		sim_scores=np.sum(y_batch * anchor_y,axis=1)
-		thres=np.minimum(sim_scores[i]-1,threshold_similarity)
-		thres=np.maximum(thres,0)
+		# get embedding distances
 		dists=np.sqrt(np.sum((embeddings-anchor)**2,axis=1))
 		distance_order=np.argsort(dists)
-		batch_order_from_distance_order=np.argsort(distance_order)
-		sim_dist_wise=sim_scores[distance_order]
-		if debug:
-			print(i,num_positives)
-			print(sim_scores)
-		# idcs are in distance order
-		negative_idcs=np.nonzero(sim_dist_wise<=thres)[0]
-		# mine negatives first
-		for j,neg_idx in enumerate(negative_idcs):
-			if j > max_negatives:
+		sim_distance_order=sim_scores[distance_order]
+		min_sim=int(np.min(sim_scores))
+		max_sim=int(np.max(sim_scores))
+		num_anchor_triplets=0
+		# choose a threshold similarity to partition the points into positives and negatives
+		# starting from the smallest similarity
+		for sim_thres in range(min_sim+1,max_sim):
+			positive_idcs=np.nonzero(sim_distance_order>=sim_thres)[0]
+			if debug:
+				print(positive_idcs)
+			# mine positives first
+			for j,pos_idx in enumerate(positive_idcs):
+				# its the anchor
+				if distance_order[pos_idx]==i:
+					continue
+				negative_idcs=np.nonzero(sim_distance_order[:pos_idx]<sim_thres)[0]
+				# no negatives ahead of this positive
+				if len(negative_idcs)==0:
+					continue
+				# get negatives (which are already in closest first order)
+				num_negatives=np.minimum(max_negatives_per_pos,negative_idcs.shape[0])
+				for _ in range(0,num_negatives):
+					# choose a negative randomly, because there are a lot of negatives
+					# and since as we go down the positive_idcs, the previous negative_idcs
+					# is included, so we don't want to keep choosing the same negatives
+					k=np.random.randint(0,len(negative_idcs))
+					neg_idx=negative_idcs[k]
+					triplets.append((i,
+									distance_order[pos_idx],
+									distance_order[neg_idx]))
+					num_anchor_triplets+=1
+					if debug:
+						print((i,distance_order[pos_idx],distance_order[neg_idx]))	
+			if num_anchor_triplets>=max_trips_per_anchor:
 				break
-			positive_idcs=np.nonzero(sim_dist_wise[neg_idx+1:]>thres)[0]
-			# end of the queue
-			if len(positive_idcs)==0:
-				break
-			positive_idcs+=neg_idx+1
-			# get positives
-			for k in range(0,np.minimum(num_positives,positive_idcs.shape[0])):
-				t=np.random.randint(0,positive_idcs.shape[0])
-				pos_idx=positive_idcs[t]
-				triplets.append((i,
-								distance_order[pos_idx],
-								distance_order[neg_idx]))
-				if debug:
-					print((i,distance_order[pos_idx],distance_order[neg_idx]))				
 	return triplets
 
-def get_triplets(embeddings,y_batch,threshold_similarity,num_positives,max_negatives,debug=False):
-	trips_list=select_triplets(embeddings.detach().numpy(),y_batch,threshold_similarity,
-								num_positives,max_negatives,debug)
+
+def get_triplets(embeddings,y_batch,max_negatives_per_pos,max_trips_per_anchor,debug=False):
+	trips_list=select_triplets(embeddings.detach().numpy(),y_batch,max_negatives_per_pos,max_trips_per_anchor,debug)
 	if len(trips_list)==0:
 		return None
 	anchors=[]
